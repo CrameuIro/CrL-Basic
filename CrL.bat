c7940632ef80fffe550663530e52b88831edfe2f
CrameuIro <2560077651@qq.com>
2026-06-10 17:15:58 +0800

    Add files via upload

---
+++ CrL.bat
@@ -0,0 +1,81 @@
+@echo off
+
+cd platform-tools
+cls
+
+adb kill-server
+adb devices
+echo Please make sure USB debugging is enabled on your device and grant this computer permission to perform ADB debugging.
+echo.
+pause
+echo.
+
+echo.
+echo Replacing... 1/3
+echo Browser (org.lineageos.jelly) -> Via (mark.via)
+echo.
+
+adb shell pm disable-user org.lineageos.jelly
+adb install ../replacing-apks/Browser.apk
+
+echo.
+echo Replacing... 2/3
+echo Camera (org.lineageos.snap / org.lineageos.aperture) -> Camera (app.grapheneos.camera)
+echo.
+
+adb shell pm disable-user org.lineageos.snap
+adb shell pm disable-user org.lineageos.aperture
+adb install ../replacing-apks/Camera.apk
+
+echo.
+echo Replacing... 3/3
+echo Music (org.lineageos.twelve) -> Booming Music (com.mardous.booming)
+echo.
+
+adb shell pm disable-user org.lineageos.twelve
+adb install ../replacing-apks/Music.apk
+
+echo.
+echo The pre-installed apps have been replaced.
+echo.
+
+echo Installing other apps... 1/5
+echo CoolApk (com.coolapk.market, arm64 only)
+echo.
+
+adb install ../other-apks/CoolApk-arm64.apk
+
+echo.
+echo Installing other apps... 2/5
+echo F-Droid Basic (org.fdroid.basic)
+echo.
+
+adb install ../other-apks/F-Droid_Basic.apk
+
+echo.
+echo Installing other apps... 3/5
+echo InstallerX Revived (com.rosan.installer.x.revived)
+echo.
+
+adb install ../other-apks/InstallerX_Revived.apk
+
+echo.
+echo Installing other apps... 4/5
+echo Localsend (org.localsend.localsend_app)
+echo.
+
+adb install ../other-apks/LocalSend-arm32.apk
+adb install ../other-apks/LocalSend-arm64.apk
+
+echo.
+echo Installing other apps... 5/5
+echo MT Manager (bin.mt.plus)
+echo.
+
+adb install ../other-apks/MT_Manager.apk
+
+echo.
+echo All essential apps have been installed.
+echo.
+pause
+exit
---
+++ README.md
@@ -0,0 +1,17 @@
+**A batch tool prepared for LineageOS or LOS-based systems. **
+
+Connect the device to Windows and run CRL.bat, some of your system applications will be disabled and replaced with better alternatives:
+
+1. Browser (org.lineageos.jelly) -> Via (mark.via)
+2. Camera (org.lineageos.aperture) -> Camera (app.grapheneos.camera)
+3. Music (org.lineageos.twelve) -> Booming Music (com.mardous.booming)
+
+In addition, several other apps will be installed on your device:
+
+1. CoolApk (com.coolapk.market, arm64 only)
+2. F-Droid Basic (org.fdroid.basic)
+3. InstallerX Revived (com.rosan.installer.x.revived)
+4. Localsend (org.localsend.localsend_app)
+5. MT Manager (bin.mt.plus)
+
+The included app packages are all the latest versions.
